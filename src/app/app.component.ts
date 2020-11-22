73b33bd06df49d122350a41c4614857ebd0d5104
Casey <casey.gardonio@foat.org>
2020-11-22 15:37:09 -0600

    updated button styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username: string = '';
+  allowUsernameReset: boolean = 'username === ""';
 
   onUsernameReset() {
     this.username = '';
